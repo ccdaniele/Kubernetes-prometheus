e047c8b55cdcfd68d2478de9bff00a462f319d0c
aldrick.castro <aldrick.castro@datadoghq.com>
2022-08-05 17:43:29 -0400

    Application image created

--- python-kube/pykube/pykube/main.py
+++ python-kube/pykube/pykube/main.py
@@ -8,12 +8,14 @@ app: FastAPI = FastAPI()
 @app.get("/")
 async def index():
     environment: str = os.environ.get("APP_ENV")
-    result: str = "From Host"
+    location: str = "From "
 
-    if environment:
-        result = "From Kubernetes"
+    if environment is None:
+        location += "Host"
+    else:
+        location += environment
         
-    return {"Hello": result}
+    return {"Hello": location}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=5050,reload=True)
